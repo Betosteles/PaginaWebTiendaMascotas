04bff37d6c0285ce6da9e8359bc0447bd078c9d1
Betosteles <72321391+Betosteles@users.noreply.github.com>
2023-04-29 14:05:24 -0600

    Add files via upload

---
+++ Database BackUp/dbstore_pedido_detalle.sql
@@ -0,0 +1,51 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dbstore
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `pedido_detalle`
+--
+
+DROP TABLE IF EXISTS `pedido_detalle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pedido_detalle` (
+  `id_pedido` int NOT NULL,
+  `producto_id` int NOT NULL,
+  `cantidad_producto` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedido_detalle`
+--
+
+LOCK TABLES `pedido_detalle` WRITE;
+/*!40000 ALTER TABLE `pedido_detalle` DISABLE KEYS */;
+INSERT INTO `pedido_detalle` VALUES (2,2,17),(8,4,1),(8,5,1),(8,2,1),(8,3,1),(9,2,1),(9,4,1),(9,5,1),(9,3,1),(10,4,1),(10,3,1),(10,2,1),(10,5,1),(11,3,1),(11,4,1),(11,5,1),(11,2,1),(14,2,1),(14,5,1),(14,3,1),(14,4,1),(15,4,1),(15,5,1),(15,3,1),(15,2,1),(16,3,1),(16,4,1),(16,5,1),(16,2,1),(17,2,1),(17,5,1),(17,4,1),(17,3,1),(19,3,1),(19,2,1),(19,5,1),(19,4,1),(21,3,1),(22,2,1),(22,3,1),(23,3,1),(23,2,1),(24,8,1),(24,4,1),(24,3,1),(24,2,1),(25,5,1),(26,6,1);
+/*!40000 ALTER TABLE `pedido_detalle` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-29 14:03:40
